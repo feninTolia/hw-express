dc52a59ad8b0cbed7994fcef14d30c7e8bf0b7bc
feninTolia <fenintolja@gmail.com>
2022-12-08 15:34:34 +0200

    addded limit, page, favorite filters

--- controllers/contacts/getAllController.js
+++ controllers/contacts/getAllController.js
@@ -2,8 +2,22 @@ const Contact = require('../../models/contacts');
 
 async function getAll(req, res) {
   const { _id: owner } = req.user;
+  const { page: skip, limit, favorite } = req.query;
 
-  const contatcsList = await Contact.find({ owner });
+  const page = skip * limit - limit;
+  const query = {
+    owner,
+  };
+
+  if (favorite) {
+    query.favorite = favorite;
+  }
+
+  console.log(query);
+
+  const contatcsList = await Contact.find(query)
+    .skip(page || 0)
+    .limit(limit || 0);
 
   res.json(contatcsList);
 }
